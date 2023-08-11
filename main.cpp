c4cdf1234ab86b55341a55de5b22168676c7955f
Rick Mondal <124441126+rickmondal-2004@users.noreply.github.com>
2023-08-11 13:02:18 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream.h>
+#include <time.h>
+#include <iomanip.h>
+#include <fstream.h>
 char f[10] = "f";
 char s[10] = "s";
 int addr, ad, flag, f1, d, m, i, amt;
