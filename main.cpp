305087c1ffc0e4d13c184da095a49558907bf6c8
Rick Mondal <124441126+rickmondal-2004@users.noreply.github.com>
2023-08-27 09:28:18 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,5 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <iostream.h>
-#include <time.h>
-#include <iomanip.h>
-#include <fstream.h>
-char f[10] = "f";
-char s[10] = "s";
-int addr, ad, flag, f1, d, m, i, amt;
-float tamt;
-class login
-{
-public:
-    char id[100];
-    char pass[100];
-    char *password;
-    void getid()
+#include
     {
         cout << "Enter your id:";
         gets(id);
